552008ff39fd2ed77c622c784b493903bdaf3f10
BriWS <briws@pm.me>
2021-06-16 17:04:33 +0300

    homepage & HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {App} from './App';
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,document.getElementById('root')
+  </HashRouter>,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
