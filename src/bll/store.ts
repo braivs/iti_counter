f1d8f7b0d1f5d0370c0db385623433991fdba1cd
BriWS <brightwiths@gmail.com>
2021-06-26 20:00:27 +0300

    counter with redux alpha progress

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -1,10 +1,17 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {counterReducer} from "./counter-reducer";
+import {loadState, saveState} from "../utils/localstorage-utils";
 
 const rootReducer = combineReducers({
   counter: counterReducer
 })
 
-export type AppStateType = ReturnType<typeof rootReducer>
+export const store = createStore(rootReducer, loadState());
 
-export const store = createStore(rootReducer);
+store.subscribe(() => {
+  saveState({
+    counter: store.getState().counter
+  })
+})
+
+export type AppStateType = ReturnType<typeof rootReducer>
